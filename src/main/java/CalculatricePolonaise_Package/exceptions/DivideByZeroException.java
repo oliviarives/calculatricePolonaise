83fabc288c14dec8669c81c16b59e78d67dc68e5
salomefaure <salome.faure79@gmail.com>
2025-11-26 12:57:05 +0100

    RPN Calc implementation + tests

---
+++ src/main/java/CalculatricePolonaise_Package/exceptions/DivideByZeroException.java
@@ -0,0 +1,8 @@
+package CalculatricePolonaise_Package.exceptions;
+
+public class DivideByZeroException extends Exception {
+    public DivideByZeroException(String message) {
+
+        super(message);
+    }
+}
--- src/test/java/Stack_Package/StackTest.java
+++
@@ -1,7 +0,0 @@
-package Stack_Package;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class StackTest {
-
-}
