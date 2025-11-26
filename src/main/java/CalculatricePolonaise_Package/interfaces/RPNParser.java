4bce367bfea62d43776043cb916ad79a930a002c
olirvs <oliviamazone@gmail.com>
2025-11-26 14:07:29 +0100

    update RPNParser.java

--- src/main/java/CalculatricePolonaise_Package/interfaces/RPNParser.java
+++ src/main/java/CalculatricePolonaise_Package/interfaces/RPNParser.java
@@ -1,7 +1,18 @@
 package CalculatricePolonaise_Package.interfaces;
 
-public interface RPNParser {
+import CalculatricePolonaise_Package.exceptions.DivideByZeroException;
+import CalculatricePolonaise_Package.exceptions.NotEnoughOperandsOnStackException;
 
-    /*double parseAndDisplayResult(String toParse) throws
-            NotEnoughOperandsOnStackException, DivideByZeroException, ParseException;*/
+import java.text.ParseException;
+
+public interface RPNParser {
+    /**
+     * parses the string, computing and display the last result
+     * @param toParse
+     * @return the last result
+     */
+    double parseAndDisplayResult(String toParse)
+            throws NotEnoughOperandsOnStackException,
+            DivideByZeroException,
+            ParseException;
 }
